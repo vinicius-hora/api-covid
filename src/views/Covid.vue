67710e87bc5a4f2435e610392e800322ac6c5292
vinicius-hora <vinicius_hora@live.com>
2021-12-29 10:58:45 -0300

    api sendo consumida

--- src/views/Covid.vue
+++ src/views/Covid.vue
@@ -7,13 +7,33 @@
             <v-btn @click="totalCasos">Mostrar dados</v-btn>
             <br>
             <br>
-            <v-row align="center">
+            <v-row >
                 <v-col cols="6">
-                    <p>Total de casos: {{casos}}</p>
-                    <p>Total de mortes: {{mortes}}</p>
+                    <p >Total de casos: {{casos}}</p>
+                    <p >Total de mortes: {{mortes}}</p>
                 </v-col>
             </v-row>
-            <v-btn @click="totalCasos">Mostrar dados por estado</v-btn>
+            <v-btn @click="casosPorEstado">Mostrar dados por estado</v-btn>
+            <br>
+            <!-- funcionando com UL -->
+            <!-- <ul >
+               
+                <li v-for="(dado, id) in dados" :key="id"> Estado: {{dado.state}}
+                    <br>
+                    Casos: {{dado.cases}}
+                    <br>
+                    mortes: {{dado.deaths}}
+                    <v-spacer></v-spacer>
+                    
+                </li>
+              
+            </ul> -->
+            <!-- <div v-for="(dado, id) in dados" :key="id">
+                <p>Estado: {{dado.state}}</p>
+                <p>Casos: {{dado.cases}}</p>
+                <p>mortes: {{dado.deaths}}</p>
+                <v-spacer></v-spacer>
+            </div> -->
         </v-container>
     </v-main>
     </v-app>
@@ -25,11 +45,22 @@
 
 export default ({
      data:() => ({
+         //   variaves da api total
        casos: 0,
        mortes: 0,
+       dados:[
+           
+       ],
+    //   variaves da api por estado
+       cases:[],
+       states:[],
+       deaths:[],
+
+       teste:0,
+       
     }),
     methods: {
-        
+        // consumo da api total de tados
         async totalCasos(){
             const response = await fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil');
             const data = await response.json();
@@ -39,8 +70,29 @@ export default ({
             console.log(this.casos);
             
         },
-        exibirCasos(){
-            console.log(this.data.data);
+        //consumo da api por estado
+        async casosPorEstado(){
+            const response = await fetch('https://covid19-brazil-api.vercel.app/api/report/v1');
+            const data = await response.json();
+            console.log("dadios ", data);
+            this.dados = data.data;
+            console.log("array: ",this.dados);
+            this.teste = this.dados[1].cases;
+            for(let i = 0; i < this.dados.length; i++){
+                this.cases.push(this.dados[i].cases);
+            }
+            for(let i = 0; i < this.dados.length; i++){
+                this.states.push(this.dados[i].state.format);
+            }
+            for(let i = 0; i < this.dados.length; i++){
+                this.deaths.push(this.dados[i].deaths);
+            }
+            
+            console.log("casos: ", this.cases);
+            console.log("mortes: ", this.deaths);
+             console.log("estado: ", this.states);
+            
+
         }
     },
         
