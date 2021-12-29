63cc48c351fcd77d0d715eca9a6ab8365948ff84
vinicius-hora <vinicius_hora@live.com>
2021-12-29 09:18:49 -0300

    consumindo api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+       <router-link to="/covid">Covid</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/Covid.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app>
+    <v-app-bar app></v-app-bar>
+
+    <v-main>
+        <v-container>
+            <v-btn @click="totalCasos">Mostrar dados</v-btn>
+            <br>
+            <br>
+            <v-row align="center">
+                <v-col cols="6">
+                    <p>Total de casos: {{casos}}</p>
+                    <p>Total de mortes: {{mortes}}</p>
+                </v-col>
+            </v-row>
+            <v-btn @click="totalCasos">Mostrar dados por estado</v-btn>
+        </v-container>
+    </v-main>
+    </v-app>
+    
+</template>
+
+<script>
+
+
+export default ({
+     data:() => ({
+       casos: 0,
+       mortes: 0,
+    }),
+    methods: {
+        
+        async totalCasos(){
+            const response = await fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil');
+            const data = await response.json();
+            this.casos = data.data.confirmed;
+            this.mortes = data.data.deaths;
+            console.log(data);
+            console.log(this.casos);
+            
+        },
+        exibirCasos(){
+            console.log(this.data.data);
+        }
+    },
+        
+    
+})
+</script>
+
+<style scoped>
+
+</style>
